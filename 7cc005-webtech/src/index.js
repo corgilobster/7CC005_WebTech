a6e881625d9a06abfabf36280414e4f622aa2b7a
corgilobster <marcusmohammadi@gmail.com>
2021-03-09 10:46:43 +0000

    Added Restfull API Tutorial

--- 7cc005-webtech/src/index.js
+++ 7cc005-webtech/src/index.js
@@ -9,13 +9,6 @@ function Square(props) {
   );
 }
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
@@ -30,8 +23,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.handleClick(i)}
       />
     );
   }
@@ -69,6 +62,16 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
   render() {
     return (
       <div className="game">
