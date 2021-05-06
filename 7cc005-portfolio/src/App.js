7e1fc43f4b0d8a352115861cfd26e401078476b7
corgilobster <marcusmohammadi@gmail.com>
2021-05-06 13:52:33 +0100

    Added Game Screen

--- 7cc005-portfolio/src/App.js
+++ 7cc005-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import Game from './Game';
 import { Component } from 'react';
 
 class App extends Component{
@@ -8,12 +9,22 @@ class App extends Component{
     super(props);
 
     this._handleLogin = this._onLogin.bind(this);
+
+    this.state = {};
   }
 
   render() {
+    const { player } = this.state;
+    let appContents;
+    /*if (player) {
+      appContents = <Game player={ player } />;
+    } else {
+      appContents = <Login handleLogin={ this._handleLogin } />;
+    }*/
+
     return (
         <div className="App container-fluid">
-         <Login handleLogin={ this._handleLogin } />
+         <Game/>
         </div>
     );
   }
