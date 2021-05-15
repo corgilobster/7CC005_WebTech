7d1cd41643ba7ce3b1a02d6168e0672b64106125
corgilobster <marcusmohammadi@gmail.com>
2021-05-15 17:43:23 +0100

    fixed routing issue

--- ci3/application/config/routes.php
+++ ci3/application/config/routes.php
@@ -49,15 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Pages/view';
-$route['(:any)'] = 'Pages/view/$1';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
-
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
-$route['(:any)'] = 'Pages/view/$1';
-$route['default_controller'] = 'Pages/view';
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
 
 $route['players'] = 'players/index';
 $route['playersCreate']['post'] = "players/register";
--- ci3/application/controllers/News.php
+++ ci3/application/controllers/News.php
@@ -26,12 +26,12 @@ class News extends CI_Controller {
 
         if (empty($data['news_item']))
         {
-            show_404();
+                show_404();
         }
 
         $data['title'] = $data['news_item']['title'];
 
-        $this->view('templates/header', $data);
+        $this->load->view('templates/header', $data);
         $this->load->view('news/view', $data);
         $this->load->view('templates/footer');
     }
