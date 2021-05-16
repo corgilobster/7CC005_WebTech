ac94dd0cbfe5cea84495c874172c27af9dff680f
corgilobster <marcusmohammadi@gmail.com>
2021-05-16 15:35:27 +0100

    added Player_model.php with functions

---
+++ ci3/application/models/Players/Player_model.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Player_model extends CI_Model
+{
+    private $player = 'player';
+
+    function check_player($name)
+    {
+        $query = $this->db->get_where($this->player, array("name" => $name));
+        if($query) return true; else return false;
+    }
+
+    function get_password($name)
+    {
+        $this->db->select('password');
+        $query = $this->db->get('player');
+        return $query->result();
+    }
+
+    function get_player($name)
+    {
+        $query = $this->db->get_where($this->player, array("name" => $name));
+        if($query)
+        {
+            return $query->result();
+        }
+        return NULL;
+    }
+
+    function login_player($name, $password)
+    {
+        if(!check_player($name)) return NULL;
+        else if( $password === get_password($player))
+        {
+            $query = $this->db->get_where($this->player, array("name" => $name));
+            return $query->result();
+        }
+        return NULL;
+    }
+
+    function add_player($name, $password)
+    {
+        if(check_player($name)) return false;
+
+        $data = array('name' => $name, 'password' => $password);
+        $this->db->insert($this->player, $data);
+        return true;
+    }
+
+    function update_current_health($name, $health)
+    {
+        $this->db->set('current_health', $health);
+        $this->db->where('name', $name);
+        $this->db->update('player');
+    }
+}
